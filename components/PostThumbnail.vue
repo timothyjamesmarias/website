73cbeb979003b222fbfe484c4ac18f76c817dd48
timothyjamesmarias <timothyjamesmarias@gmail.com>
2022-11-18 16:30:00 -0800

    styled index page

---
+++ components/PostThumbnail.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+   post: {
+    Type: Array,
+    default: undefined,
+    required: true,
+   }, 
+})
+console.log(article)
+</script>
+<template>
+    <div class="rounded-lg">
+        <NuxtLink to=post._path>
+            <div class="text-white">
+                {{ post.title }}
+            </div>
+        </NuxtLink>
+    </div>
+</template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,1 +1,18 @@
-<template></template>
+<script setup lang="ts">
+
+</script>
+<template>
+    <ContentList path="/posts" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path" >
+        <div class="py-3">
+            <NuxtLink :to="article._path">
+               <div class="text-white font-ubuntu ">
+                  <h2 class="text-xl hover:text-blue">{{ article.title }}</h2> 
+                  <p>{{ article.description }}</p>
+                  <ContentLine />
+               </div> 
+            </NuxtLink>
+        </div> 
+      </div>
+    </ContentList>
+</template>
