5a39e9983dda6d88e4ca82c0cfd11dd201d23024
Timothy Marias <timothyjamesmarias@gmail.com>
2023-02-26 15:31:00 -0800

    added daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,23 +9,10 @@ export default <Config>{
   ],
   theme: {
     extend: {
-      colors: {
-        "background": "#282a36",
-        "secondary": "#44475a",
-        "main": "#f8f8f2",
-        "highlight": "#6272a4",
-        "cyan": "#8be9fd",
-        "green": "#50fa7b",
-        "orange": "#ffb86c",
-        "pink": "#ff79c6",
-        "purple": "#bd93f9",
-        "red": "#ff5555",
-        "yellow": "#f1fa8c",
-      },
       fontFamily: {
         "main": ["Iosevka", "monospace"],
       }
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
