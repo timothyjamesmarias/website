0877421c30e53460386ca88c4390966ae4c1e8a1
Timothy Marias <timothyjamesmarias@gmail.com>
2023-02-28 15:15:20 -0800

    navbar complete

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -21,29 +21,29 @@
       </NavItem>
     </div>
     <div class="flex-none sm:hidden dropdown dropdown-end">
-      <label class="text-3xl hover:text-secondary-content">
+      <label tabindex="0" class="text-3xl hover:text-secondary-content">
         <font-awesome-icon :icon="['fas', 'bars']" class=""/>
       </label>
-      <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box">
+      <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-300 rounded-box">
         <li>
-        <NavItem :href="'/#contact'">
-            <font-awesome-icon :icon="['fas', 'envelope']" />
-        </NavItem>
+          <NavItem :href="'/#contact'">
+              <font-awesome-icon :icon="['fas', 'envelope']" />
+          </NavItem>
         </li>
         <li>
-        <NavItem :href="'/blog'" class="ml-6">
-            <font-awesome-icon :icon="['fas', 'book']" />
-        </NavItem>
+          <NavItem :href="'/blog'">
+              <font-awesome-icon :icon="['fas', 'book']" />
+          </NavItem>
         </li>
         <li>
-        <NavItem :href="'/rss'" class="ml-6">
-            <font-awesome-icon :icon="['fas', 'rss']" />
-        </NavItem>
+          <NavItem :href="'/rss'">
+              <font-awesome-icon :icon="['fas', 'rss']" />
+          </NavItem>
         </li>
         <li>
-        <NavItem :href="'https://github.com/timothyjamesmarias'" class="ml-6">
-            <font-awesome-icon :icon="['fab', 'github']" />
-        </NavItem>
+          <NavItem :href="'https://github.com/timothyjamesmarias'">
+              <font-awesome-icon :icon="['fab', 'github']" />
+          </NavItem>
         </li>
       </ul>
     </div>
