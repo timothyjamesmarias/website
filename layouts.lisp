fb2a00df1d1184524276b96d9eb8bf3915513732
timothyjamesmarias <timothyjamesmarias@gmail.com>
2026-06-19 21:59:14 -0700

    feat: layouts and some better naming

--- layouts.lisp
+++ layouts.lisp
@@ -1,1 +1,11 @@
 (in-package #:website)
+
+(defun default-layout (content)
+  (with-html-string
+      (:doctype)
+    (:html
+     (:head
+     (:title "title string"))
+     (:header (:p "header text"))
+    (:main (:raw content))
+    (:footer (:p "footer text")))))
--- ssg.lisp
+++ ssg.lisp
@@ -6,26 +6,26 @@
 (defun markdown-file-p (path)
   (string-equal (pathname-type path) "md"))
 
-(defun md-to-html (markdown-string)
+(defun convert-md-to-html (markdown-string)
   (with-output-to-string (stream)
     (3bmd:parse-string-and-print-to-stream markdown-string stream)))
 
 (defun get-content-from-file (file)
   (uiop:read-file-string file))
 
-(defun html-file-name (file)
+(defun get-html-file-name (file)
   (let* ((fname (concatenate 'string (pathname-name file) ".html"))
     (output-name (concatenate 'string *output-dir* fname)))
     output-name))
 
 (defun build-page (file)
   (let* ((md-file-contents (get-content-from-file file))
-         (converted-html-contents (md-to-html md-file-contents)))
-    converted-html-contents))
+         (converted-html-contents (convert-md-to-html md-file-contents)))
+    (default-layout converted-html-contents)))
 
 (defun build-html-page (file)
   (ensure-directories-exist *output-dir*)
-  (with-open-file (stream (html-file-name file)
+  (with-open-file (stream (get-html-file-name file)
                           :direction :output
                           :if-exists :supersede
                           :if-does-not-exist :create)
